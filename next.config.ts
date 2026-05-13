63bac9b68650796d697a78192932203c82be331d
Developer <dev@riman.com>
2026-05-13 22:50:22 +0400

    feat: scaffold Next.js 15 project with Prisma, Tailwind, shadcn/ui, next-intl

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from "next";
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {};
+
+export default withNextIntl(nextConfig);
