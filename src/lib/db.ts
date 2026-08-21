2edf63f8e602a60f8f73d0b2d74bc3413fedfe7c
Developer <dev@riman.com>
2026-08-21 12:15:59 +0400

    chore: tune prisma client logging for production

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,27 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, type Prisma } from '@prisma/client'
+
+import { logger } from '@/lib/logger'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db = globalForPrisma.prisma ?? new PrismaClient()
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === 'production'
+        ? [{ emit: 'event', level: 'error' }]
+        : ['warn', 'error'],
+  })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV === 'production') {
+  ;(db as PrismaClient<Prisma.PrismaClientOptions, 'error'>).$on(
+    'error',
+    (e) => {
+      logger.error(e.message)
+    }
+  )
+} else {
+  globalForPrisma.prisma = db
+}
